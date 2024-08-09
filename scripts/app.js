75ab91d95a5a4c6a1f9292ccf1a0ff496e38c664
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-08-09 10:17:09 +0100

    Play audio file by clicking button

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,16 @@
-
 /*-------------------------------- Constants --------------------------------*/
-
-
 
 /*-------------------------------- Variables --------------------------------*/
 
-
-
 /*------------------------ Cached Element References ------------------------*/
-
-
-
+const playerButtons = Array.from(document.querySelectorAll("#player > div"));
+const audio = document.querySelector("#audio");
 /*-------------------------------- Functions --------------------------------*/
 
-
-
 /*----------------------------- Event Listeners -----------------------------*/
-
+playerButtons.forEach((playerButton) => {
+  playerButton.addEventListener("click", (e) => {
+    audio.src = `sounds/${e.target.id}.wav`;
+    audio.play();
+  });
+});
