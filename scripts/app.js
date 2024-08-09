92fa30dd1a019f797ce184767d172debf44184f2
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-08-09 11:28:23 +0100

    Make Randomize button play random audio

--- scripts/app.js
+++ scripts/app.js
@@ -5,12 +5,23 @@
 /*------------------------ Cached Element References ------------------------*/
 const playerButtons = Array.from(document.querySelectorAll("#player > div"));
 const audio = document.querySelector("#audio");
+const randomBtn = document.querySelector("#randomizer");
 /*-------------------------------- Functions --------------------------------*/
+function playAudio(audioEl) {
+  audio.src = `sounds/${audioEl.id}.wav`;
+  audio.play();
+}
 
+function playRandomAudio(e) {
+  const randomIdx = Math.floor(Math.random() * playerButtons.length);
+  const randomAudio = playerButtons[randomIdx];
+  playAudio(randomAudio);
+}
 /*----------------------------- Event Listeners -----------------------------*/
 playerButtons.forEach((playerButton) => {
   playerButton.addEventListener("click", (e) => {
-    audio.src = `sounds/${e.target.id}.wav`;
-    audio.play();
+    playAudio(e.target);
   });
 });
+
+randomBtn.addEventListener("click", playRandomAudio);
